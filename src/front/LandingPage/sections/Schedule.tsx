a4e0bd731266b401d24cf6ca232da574b212adc2
Antonio Rodarte <tonysolo94@hotmail.com>
2024-03-19 22:52:19 -0600

    Beautify code

--- src/front/LandingPage/sections/Schedule.tsx
+++ src/front/LandingPage/sections/Schedule.tsx
@@ -6,35 +6,27 @@ const Schedule = () => {
   const flexProps = {
     display: "flex",
     alignItems: "center",
-    justifyContent: 'center',
+    justifyContent: "center",
     flexDirection: "column",
   };
 
-  const mobileFlexProps = flexProps && {
-    
-  }
+  const mobileFlexProps = flexProps && {};
   return (
     <>
-
       <div>
-
-
-
-
-        <Grid container sx={{  display: { xs: 'flex', md: 'none' } }}>
-            <Grid xs={12} {...mobileFlexProps}>
-                <Grid container>
-                    <Grid xs={5.8} {...flexProps}>
-                    <h5>Misa</h5>
-                    <img src="church.png" alt="" />
-                  </Grid>
-                  <Grid
-                    xs={0.2}
-                    className={styles.pinkDivider}
-                    {...flexProps}
-                    ></Grid>
-                    <Grid xs={5.8} {...flexProps}>
-    
+        <Grid container sx={{ display: { xs: "flex", md: "none" } }}>
+          <Grid xs={12} {...mobileFlexProps}>
+            <Grid container>
+              <Grid xs={5.8} {...flexProps}>
+                <h5>Misa</h5>
+                <img src="church.png" alt="" />
+              </Grid>
+              <Grid
+                xs={0.2}
+                className={styles.pinkDivider}
+                {...flexProps}
+              ></Grid>
+              <Grid xs={5.8} {...flexProps}>
                 <h6>7:00 p.m.</h6>
                 <p>
                   Parroquia Nuestra
@@ -43,161 +35,110 @@ const Schedule = () => {
                 <p>
                   C. Rancho el Retiro Sur 3517 <br /> Juárez, Chih.
                 </p>
-              
-                    </Grid>
-
-                </Grid>
+              </Grid>
             </Grid>
-
-
-
-
-
-
-
-
-            <Grid xs={12} {...mobileFlexProps}>
-                <Grid container>
-                    <Grid xs={5.8} {...flexProps}>
-                    <h5>Cena</h5>
-                    <img src="dinner.png" alt="" />
-                  </Grid>
-                  <Grid
-                    xs={0.2}
-                    className={styles.pinkDivider}
-                    {...flexProps}
-                    ></Grid>
-                    <Grid xs={5.8} {...flexProps}>
-                <h6>7:00 p.m.</h6>
-                <p>
-                  Parroquia Nuestra
-                  <br /> Señora de la Paz
-                </p>
-                <p>
-                  C. Rancho el Retiro Sur 3517 <br /> Juárez, Chih.
-                </p>
-                    </Grid>
-                </Grid>
-            </Grid>
-
-
-
-
-
-
-
-
-
-            <Grid xs={12} {...mobileFlexProps}>
-                <Grid container>
-                    <Grid xs={5.8} {...flexProps}>
-                    <h5>Presentación</h5>
-                    <img src="dance.png" alt="" />
-                  </Grid>
-                  <Grid
-                    xs={0.2}
-                    className={styles.pinkDivider}
-                    {...flexProps}
-                    ></Grid>
-                    <Grid xs={5.8} {...flexProps}>
-                <h6>7:00 p.m.</h6>
-                <p>
-                  Parroquia Nuestra
-                  <br /> Señora de la Paz
-                </p>
-                <p>
-                  C. Rancho el Retiro Sur 3517 <br /> Juárez, Chih.
-                </p>
-                    </Grid>
-                </Grid>
-            </Grid>
-
-
-
-
-
-
-
-
-
-
-            <Grid xs={12} {...mobileFlexProps}>
-                <Grid container>
-                    <Grid xs={5.8} {...flexProps}>
-                    <h5>Fiesta</h5>
-                    <img src="discoBall.png" alt="" />
-                  </Grid>
-                  <Grid
-                    xs={0.2}
-                    className={styles.pinkDivider}
-                    {...flexProps}
-                    ></Grid>
-                    <Grid xs={5.8} {...flexProps}>
-                <h6>7:00 p.m.</h6>
-                <p>
-                  Parroquia Nuestra
-                  <br /> Señora de la Paz
-                </p>
-                <p>
-                  C. Rancho el Retiro Sur 3517 <br /> Juárez, Chih.
-                </p>
-                    </Grid>
-                </Grid>
-            </Grid>
-
-
-
-
-
-
-
-
-
-
-            <Grid xs={12} {...mobileFlexProps}>
-                <Grid container>
-                    <Grid xs={5.8} {...flexProps}>
-                    <img src="justMarried.png" alt="" />
-                  </Grid>
-                  <Grid
-                    xs={0.2}
-                    className={styles.pinkDivider}
-                    {...flexProps}
-                    ></Grid>
-                    <Grid xs={5.8} {...flexProps}>
-                <h6>2:00 a.m.</h6>
-                <p>Fuímonos a koger</p>
-                    </Grid>
-                </Grid>
-            </Grid>
-            
-
-            <Grid xs={12} {...flexProps}>
-            <h4>Forever</h4>
           </Grid>
 
+          <Grid xs={12} {...mobileFlexProps}>
+            <Grid container>
+              <Grid xs={5.8} {...flexProps}>
+                <h5>Cena</h5>
+                <img src="dinner.png" alt="" />
+              </Grid>
+              <Grid
+                xs={0.2}
+                className={styles.pinkDivider}
+                {...flexProps}
+              ></Grid>
+              <Grid xs={5.8} {...flexProps}>
+                <h6>7:00 p.m.</h6>
+                <p>
+                  Parroquia Nuestra
+                  <br /> Señora de la Paz
+                </p>
+                <p>
+                  C. Rancho el Retiro Sur 3517 <br /> Juárez, Chih.
+                </p>
+              </Grid>
+            </Grid>
+          </Grid>
 
+          <Grid xs={12} {...mobileFlexProps}>
+            <Grid container>
+              <Grid xs={5.8} {...flexProps}>
+                <h5>Presentación</h5>
+                <img src="dance.png" alt="" />
+              </Grid>
+              <Grid
+                xs={0.2}
+                className={styles.pinkDivider}
+                {...flexProps}
+              ></Grid>
+              <Grid xs={5.8} {...flexProps}>
+                <h6>7:00 p.m.</h6>
+                <p>
+                  Parroquia Nuestra
+                  <br /> Señora de la Paz
+                </p>
+                <p>
+                  C. Rancho el Retiro Sur 3517 <br /> Juárez, Chih.
+                </p>
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <Grid xs={12} {...mobileFlexProps}>
+            <Grid container>
+              <Grid xs={5.8} {...flexProps}>
+                <h5>Fiesta</h5>
+                <img src="discoBall.png" alt="" />
+              </Grid>
+              <Grid
+                xs={0.2}
+                className={styles.pinkDivider}
+                {...flexProps}
+              ></Grid>
+              <Grid xs={5.8} {...flexProps}>
+                <h6>7:00 p.m.</h6>
+                <p>
+                  Parroquia Nuestra
+                  <br /> Señora de la Paz
+                </p>
+                <p>
+                  C. Rancho el Retiro Sur 3517 <br /> Juárez, Chih.
+                </p>
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <Grid xs={12} {...mobileFlexProps}>
+            <Grid container>
+              <Grid xs={5.8} {...flexProps}>
+                <img src="justMarried.png" alt="" />
+              </Grid>
+              <Grid
+                xs={0.2}
+                className={styles.pinkDivider}
+                {...flexProps}
+              ></Grid>
+              <Grid xs={5.8} {...flexProps}>
+                <h6>2:00 a.m.</h6>
+                <p>Fuímonos a koger</p>
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <Grid xs={12} {...flexProps}>
+            <h4>Forever</h4>
+          </Grid>
         </Grid>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        <Grid container 	sx={{
-          display: { xs: 'none', md: 'flex' },
-        }}>
-         
-         
+        <Grid
+          container
+          sx={{
+            display: { xs: "none", md: "flex" },
+          }}
+        >
           <Grid lg={10} sm={12}>
             <Grid container>
               <Grid md={2.4} sm={12} {...flexProps}>
@@ -272,8 +213,6 @@ const Schedule = () => {
               </Grid>
             </Grid>
           </Grid>
-
-
 
           <Grid lg={2} sm={12} {...flexProps}>
             <h4>Forever</h4>
