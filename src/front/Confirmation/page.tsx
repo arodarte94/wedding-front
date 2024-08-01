e112705c4c6439f47be7e9b4245650f12f50aa5a
Antonio Rodarte <tonysolo94@hotmail.com>
2024-08-01 00:07:55 -0600

    WIp confcirmation page

--- src/front/Confirmation/page.tsx
+++ src/front/Confirmation/page.tsx
@@ -1,19 +1,51 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import styles from "../styles/front.module.scss";
-import { Button } from "@mui/material";
+import confirmationStyles from "../styles/confirmation.module.scss";
+import { Box, Button, TextField } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const WeddingConfirmation = () => {
+  const [code, setCode] = useState(Array(5).fill("")); // Initialize state for the code
+  const refs = useRef([]);
+
+  const handleInputChange = (e, index) => {
+    const newValue = e.target.value;
+
+    // Update the state with the new value at the correct index
+    const newCode = [...code];
+    newCode[index] = newValue;
+    setCode(newCode);
+
+    if (newValue.length === 1 && index < refs.current.length - 1) {
+      refs.current[index + 1].focus();
+    }
+
+    if (index === refs.current.length - 1) {
+      // Trigger the function when the last input field is filled
+      handleSubmit(newCode);
+    }
+  };
+
+  const handleSubmit = (finalCode) => {
+    if (finalCode.every((char) => char !== "")) {
+      console.log("Code entered:", finalCode.join(""));
+      // Add your logic here (e.g., submit the code)
+    } else {
+      console.log("Please complete all fields.");
+      // Optionally, provide user feedback for incomplete code entry
+    }
+  };
+
   return (
-    <div className={styles.main}>
-      <div className={styles.sideMenu}>
-        <div className={styles.menuTitle}>
+    <Box className={styles.main}>
+      <Box className={styles.sideMenu}>
+        <Box className={styles.menuTitle}>
           <Link to="/">
             <Button className={styles.mainConfirm} variant="outlined">
               REGRESAR
             </Button>
           </Link>
-        </div>
+        </Box>
         <ul>
           <li>
             <Link to="/">Inicio</Link>
@@ -31,8 +63,30 @@ const WeddingConfirmation = () => {
             <Link to="/#gifts">Regalos</Link>
           </li>
         </ul>
-      </div>
-    </div>
+      </Box>
+
+      <Box className={styles.content}>
+        <Box className={confirmationStyles.confirmationCode}>
+          {code.map((value, index) => (
+            <TextField
+              key={index}
+              value={value}
+              inputRef={(el) => (refs.current[index] = el)}
+              onChange={(e) => handleInputChange(e, index)}
+              inputProps={{ maxLength: 1, style: { textAlign: 'center' } }}
+              className={confirmationStyles.confirmationCodeInput}
+              sx={{
+                input: {
+                    backgroundColor: '#f6f3ed',
+                    fontFamily: 'chapaza',
+                    color: ''
+                }
+              }}
+            />
+          ))}
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
