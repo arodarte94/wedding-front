23cdd676ed5195926721eef3fcaf03a50362f1b2
Antonio Rodarte <tonysolo94@hotmail.com>
2024-08-04 20:25:41 -0600

    WIP guests page

--- src/front/Confirmation/components/Guests.tsx
+++ src/front/Confirmation/components/Guests.tsx
@@ -2,7 +2,10 @@ import styles from "../../styles/front.module.scss";
 import confirmationStyles from "../../styles/confirmation.module.scss";
 import {
   Box,
+  Button,
+  ButtonGroup,
   Grid,
+  TextField,
   ToggleButton,
   ToggleButtonGroup,
   Typography,
@@ -12,221 +15,248 @@ import SideMenu from "./SideMenu";
 import _ from "lodash";
 
 const Guests = ({ guest, setGuest }) => {
+  const availableSlots = guest?.slots - guest?.guests.length;
+  console.log(availableSlots);
+  const newGuests = Array(availableSlots).fill({
+    id: null,
+    name: "",
+    confirmed: 0,
+    entree_id: 0,
+    dinner_id: 0,
+    new: true,
+  });
+
   const changeMainSelection = (e, value, property) => {
-    if(value !== null) {
-        setGuest({ ...guest, [property]: value });
+    if (value !== null) {
+      setGuest({ ...guest, [property]: value });
     }
   };
 
   const changeGuestSelection = (e, value, property, id = null) => {
-
-    if(value !== null) {
-        const guestCopy = _.cloneDeep(guest);
-        const modifiedGuest = guestCopy?.guests.find((i) => i.id === id);
-        modifiedGuest[property] = value;
-        setGuest(guestCopy);
+    if (value !== null) {
+      const guestCopy = _.cloneDeep(guest);
+      const modifiedGuest = guestCopy?.guests.find((i) => i.id === id);
+      modifiedGuest[property] = value;
+      setGuest(guestCopy);
     }
+  };
+
+  const addGuest = (e, value) => {
+    if (!value || value.trim() === "" || value.trim().length < 3) {
+      return;
+    }
+
+    const newGuestData = {
+      id: Math.random().toString(36).substr(2, 9),
+      name: value,
+      new: true,
+      confirmed: 0,
+      entree_id: 0,
+      dinner_id: 0,
+    };
+    const guestCopy = _.cloneDeep(guest);
+    guestCopy.guests.push(newGuestData);
+    setGuest(guestCopy);
   };
 
   return (
     <Box className={styles.main}>
-      
-      
-      <MobileMenu />
-      <SideMenu />
+      <Box className={confirmationStyles.footerButtons}>
+        <ButtonGroup fullWidth>
+          <Button variant="contained" color="success">
+            Guardar
+          </Button>
+          <Button variant="contained" color="warning">
+            Regresar
+          </Button>
+        </ButtonGroup>
+      </Box>
 
+      <SideMenu />
 
       <Box className={styles.content}>
         <Box className={confirmationStyles.guestsList}>
           <Grid container>
-            <Grid item xl={4} lg={6} xs={12} padding={1}>
-              <Box className={confirmationStyles.guestCard}>
-                <Typography className={confirmationStyles.guestName}>
-                  {guest.name}
-                </Typography>
-
-                <Box className={confirmationStyles.selectionRow}>
-                  <Typography className={confirmationStyles.selectionTitle}>
-                    ¿Esta persona asistirá?
-                  </Typography>
-                  <ToggleButtonGroup
-                    color="primary"
-                    value={guest.confirmed}
-                    exclusive
-                    onChange={(e, value) => {
-                      changeMainSelection(e, value, "confirmed");
-                    }}
-                    aria-label="Platform"
-                    fullWidth
-                  >
-                    <ToggleButton value={1} className={guest.confirmed ? confirmationStyles.selected : ""}>Sí</ToggleButton>
-                    <ToggleButton value={0} className={guest.confirmed ? confirmationStyles.selected : ""}>No</ToggleButton>
-                  </ToggleButtonGroup>
-                </Box>
-
-                {guest.confirmed === 1 && (
-                  <>
-                    <Box className={confirmationStyles.selectionRow}>
-                      <Typography className={confirmationStyles.selectionTitle}>
-                        Entrada
-                      </Typography>
-                      <ToggleButtonGroup
-                        color="primary"
-                        value={guest.entree_id}
-                        exclusive
-                        onChange={(e, value) => {
-                          changeMainSelection(e, value, "entree_id");
-                        }}
-                        aria-label="Entree"
-                        fullWidth
-                      >
-                        <ToggleButton value={1} className={guest.entree_id === 1 ? confirmationStyles.selected : ""}>Ensalada Olimpia</ToggleButton>
-                        <ToggleButton value={2} className={guest.entree_id === 2 ? confirmationStyles.selected : ""}>
-                          Tacos de jícama con camarón
-                        </ToggleButton>
-                      </ToggleButtonGroup>
-                    </Box>
-
-                    <Box className={confirmationStyles.selectionRow}>
-                      <Typography className={confirmationStyles.selectionTitle}>
-                        Plato fuerte
-                      </Typography>
-                      <ToggleButtonGroup
-                        color="primary"
-                        value={guest.dinner_id}
-                        exclusive
-                        onChange={(e, value) => {
-                          changeMainSelection(e, value, "dinner_id");
-                        }}
-                        aria-label="Dinner"
-                        fullWidth
-                      >
-                        <ToggleButton value={1} className={guest.dinner_id === 1 ? confirmationStyles.selected : ""}>Chile en Nogada</ToggleButton>
-                        <ToggleButton value={2} className={guest.dinner_id === 2 ? confirmationStyles.selected : ""}>
-                          Pollo con salsa en nuez
-                        </ToggleButton>
-                        <ToggleButton value={3} className={guest.dinner_id === 3 ? confirmationStyles.selected : ""}>
-                          Pizza (sólo niños)
-                        </ToggleButton>
-                      </ToggleButtonGroup>
-                    </Box>
-                  </>
-                )}
-
-                <Box className={confirmationStyles.floatingNumber}>1</Box>
-              </Box>
-            </Grid>
-
+            <GuestOptions
+              key={"mainGuest"}
+              guest={guest}
+              handle={changeMainSelection}
+              index={1}
+            />
             {guest?.guests.map((person, index) => {
               return (
-                <Grid item xl={4} lg={6} xs={12} padding={1}>
-                  <Box className={confirmationStyles.guestCard}>
-                    <Typography className={confirmationStyles.guestName}>
-                      {person.name}
-                    </Typography>
+                <GuestOptions
+                  key={"childGuest" + index}
+                  guest={person}
+                  handle={changeGuestSelection}
+                  index={index + 2}
+                />
+              );
+            })}
 
-                    <Box className={confirmationStyles.selectionRow}>
-                      <Typography className={confirmationStyles.selectionTitle}>
-                        ¿Esta persona asistirá?
-                      </Typography>
-                      <ToggleButtonGroup
-                        color="primary"
-                        value={person.confirmed}
-                        exclusive
-                        onChange={(e, value) => {
-                          changeGuestSelection(
-                            e,
-                            value,
-                            "confirmed",
-                            person.id,
-                          );
-                        }}
-                        aria-label="Confirmation"
-                        fullWidth
-                      >
-                        <ToggleButton value={1} className={person.confirmed ? confirmationStyles.selected : ""}>Sí</ToggleButton>
-                        <ToggleButton value={0} className={!person.confirmed ? confirmationStyles.selected : ""}>No</ToggleButton>
-                      </ToggleButtonGroup>
-                    </Box>
-
-                    {person.confirmed === 1 && (
-                      <>
-                        <Box className={confirmationStyles.selectionRow}>
-                          <Typography
-                            className={confirmationStyles.selectionTitle}
-                          >
-                            Entrada
-                          </Typography>
-                          <ToggleButtonGroup
-                            color="primary"
-                            value={person.entree_id}
-                            exclusive
-                            onChange={(e, value) => {
-                              changeGuestSelection(
-                                e,
-                                value,
-                                "entree_id",
-                                person.id,
-                              );
-                            }}
-                            aria-label="Entree"
-                            fullWidth
-                          >
-                            <ToggleButton value={1} className={person.entree_id === 1 ? confirmationStyles.selected : ""}>
-                              Ensalada Olimpia
-                            </ToggleButton>
-                            <ToggleButton value={2} className={person.entree_id === 2 ? confirmationStyles.selected : ""}>
-                              Tacos de jícama con camarón
-                            </ToggleButton>
-                          </ToggleButtonGroup>
-                        </Box>
-
-                        <Box className={confirmationStyles.selectionRow}>
-                          <Typography
-                            className={confirmationStyles.selectionTitle}
-                          >
-                            Plato fuerte
-                          </Typography>
-                          <ToggleButtonGroup
-                            color="primary"
-                            value={person.dinner_id}
-                            exclusive
-                            onChange={(e, value) => {
-                              changeGuestSelection(
-                                e,
-                                value,
-                                "dinner_id",
-                                person.id,
-                              );
-                            }}
-                            aria-label="Dinner"
-                            fullWidth
-                          >
-                            <ToggleButton value={1} className={person.dinner_id === 1 ? confirmationStyles.selected : ""}>
-                              Chile en Nogada
-                            </ToggleButton>
-                            <ToggleButton value={2} className={person.dinner_id === 2 ? confirmationStyles.selected : ""}>
-                              Pollo con salsa en nuez
-                            </ToggleButton>
-                            <ToggleButton value={3} className={person.dinner_id === 3 ? confirmationStyles.selected : ""}>
-                              Pizza (sólo niños)
-                            </ToggleButton>
-                          </ToggleButtonGroup>
-                        </Box>
-                      </>
-                    )}
-
-                    <Box className={confirmationStyles.floatingNumber}>
-                      {index + 2}
-                    </Box>
-                  </Box>
-                </Grid>
+            {newGuests.map((person, index) => {
+              return (
+                <NewGuest
+                  key={"newGuest" + index}
+                  guest={person}
+                  handle={changeGuestSelection}
+                  index={guest?.guests.length + index + 2}
+                  add={addGuest}
+                />
               );
             })}
           </Grid>
         </Box>
       </Box>
     </Box>
+  );
+};
+
+const GuestOptions = ({ guest, handle, index }) => {
+  return (
+    <Grid item xl={4} lg={6} xs={12} padding={1}>
+      <Box className={confirmationStyles.guestCard}>
+        <Typography className={confirmationStyles.guestName}>
+          {guest.name}
+        </Typography>
+
+        <Box className={confirmationStyles.selectionRow}>
+          <Typography className={confirmationStyles.selectionTitle}>
+            ¿Esta persona asistirá?
+          </Typography>
+          <ToggleButtonGroup
+            color="primary"
+            value={guest.confirmed}
+            exclusive
+            onChange={(e, value) => {
+              handle(e, value, "confirmed", guest.id);
+            }}
+            aria-label="Confirmation"
+            fullWidth
+          >
+            <ToggleButton
+              value={1}
+              className={guest.confirmed ? confirmationStyles.selected : ""}
+            >
+              Sí
+            </ToggleButton>
+            <ToggleButton
+              value={0}
+              className={!guest.confirmed ? confirmationStyles.selected : ""}
+            >
+              No
+            </ToggleButton>
+          </ToggleButtonGroup>
+        </Box>
+
+        {guest.confirmed === 1 && (
+          <>
+            <Box className={confirmationStyles.selectionRow}>
+              <Typography className={confirmationStyles.selectionTitle}>
+                Entrada
+              </Typography>
+              <ToggleButtonGroup
+                color="primary"
+                value={guest.entree_id}
+                exclusive
+                onChange={(e, value) => {
+                  handle(e, value, "entree_id", guest.id);
+                }}
+                aria-label="Entree"
+                fullWidth
+              >
+                <ToggleButton
+                  value={1}
+                  className={
+                    guest.entree_id === 1 ? confirmationStyles.selected : ""
+                  }
+                >
+                  Ensalada Olimpia
+                </ToggleButton>
+                <ToggleButton
+                  value={2}
+                  className={
+                    guest.entree_id === 2 ? confirmationStyles.selected : ""
+                  }
+                >
+                  Tacos de jícama con camarón
+                </ToggleButton>
+              </ToggleButtonGroup>
+            </Box>
+
+            <Box className={confirmationStyles.selectionRow}>
+              <Typography className={confirmationStyles.selectionTitle}>
+                Plato fuerte
+              </Typography>
+              <ToggleButtonGroup
+                color="primary"
+                value={guest.dinner_id}
+                exclusive
+                onChange={(e, value) => {
+                  handle(e, value, "dinner_id", guest.id);
+                }}
+                aria-label="Dinner"
+                fullWidth
+              >
+                <ToggleButton
+                  value={1}
+                  className={
+                    guest.dinner_id === 1 ? confirmationStyles.selected : ""
+                  }
+                >
+                  Chile en Nogada
+                </ToggleButton>
+                <ToggleButton
+                  value={2}
+                  className={
+                    guest.dinner_id === 2 ? confirmationStyles.selected : ""
+                  }
+                >
+                  Pollo con salsa en nuez
+                </ToggleButton>
+                <ToggleButton
+                  value={3}
+                  className={
+                    guest.dinner_id === 3 ? confirmationStyles.selected : ""
+                  }
+                >
+                  Pizza (sólo niños)
+                </ToggleButton>
+              </ToggleButtonGroup>
+            </Box>
+          </>
+        )}
+
+        <Box className={confirmationStyles.floatingNumber}>{index}</Box>
+      </Box>
+    </Grid>
+  );
+};
+
+const NewGuest = ({ guest, handle, index, add }) => {
+  return (
+    <Grid item xl={4} lg={6} xs={12} padding={1}>
+      <Box className={confirmationStyles.guestCard}>
+        <Typography className={confirmationStyles.guestName}>
+          Nuevo invitado
+        </Typography>
+        <Box className={confirmationStyles.selectionRow}>
+          <Typography className={confirmationStyles.selectionTitle}>
+            Escribe el nombre del invitado y pulsa Enter
+          </Typography>
+          <TextField
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                add(e, e.target.value);
+              }
+            }}
+            label="Nombre..."
+            fullWidth
+          />
+        </Box>
+        <Box className={confirmationStyles.floatingNumber}>{index}</Box>
+      </Box>
+    </Grid>
   );
 };
 
